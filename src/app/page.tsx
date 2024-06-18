b4e6c34072adaf597d7615601cbb6bb212f66f87
Holly Gunning <holly.jade.gunning@gmail.com>
2024-06-19 00:48:06 +0100

    Added BgVisualEffect to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import Footer from "@/components/Footer";
-import HeroSection from "@/components/HeroSection";
+import BgVisualEffect from "@/components/BgVisualEffect";
 import NavBar from "@/components/NavBar";
+import HeroSection from "@/components/HeroSection";
 
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center overflow-hidden mx-auto sm:px-10 px-5 min-h-screen lg:min-h-[75vh] bg-black-100">
-      
-      <div className="max-w-7xl">
+    <main className="relative flex justify-center overflow-hidden mx-auto sm:px-10 px-5 min-h-screen bg-black-100">
+      < BgVisualEffect /> 
+      <div className="max-w-7xl -mt-14 sm:-mt-10 md:-mt-5 lg:mt-0">
         {/* <NavBar/> */}
-      
+        
         <HeroSection/>
 
         {/* <Footer/> */}
+        
       </div>
     </main>
   );
