7ec4f9ddf001bf43c3c679084a560ebec69b7ba5
Holly Gunning <holly.jade.gunning@gmail.com>
2024-06-19 00:47:38 +0100

    Created background visual mouse effect

---
+++ src/components/BgVisualEffect.tsx
@@ -0,0 +1,31 @@
+"use client"; // Ensure this directive is at the top
+import React, { useEffect, useRef } from 'react';
+
+const BgVisualEffect: React.FC = () => {
+const backgroundRef = useRef<HTMLDivElement>(null);
+
+useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+    if (backgroundRef.current) {
+    const { clientX, clientY } = event;
+    const { innerWidth, innerHeight } = window;
+    const xPos = (clientX / innerWidth) * 100;
+    const yPos = (clientY / innerHeight) * 100;
+    backgroundRef.current.style.background = `radial-gradient(circle at ${xPos}% ${yPos}%, rgba(147, 112, 219, 0.3), transparent 30%)`;
+    }
+  };
+  window.addEventListener('mousemove', handleMouseMove);
+
+  return () => {
+    window.removeEventListener('mousemove', handleMouseMove);
+  };
+}, []);
+
+return (
+  <div className="visual-effect-container">
+    <div ref={backgroundRef} className="background-effect"></div>
+  </div>
+);
+};
+
+export default BgVisualEffect;
