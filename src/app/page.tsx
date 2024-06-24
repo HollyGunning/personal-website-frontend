e15ea547bde784bc76b38f966743b8aee7757217
Holly Gunning <holly.jade.gunning@gmail.com>
2024-06-24 16:39:33 +0100

    Main adjustments for display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 import BgVisualEffect from "@/components/BgVisualEffect";
 import NavBar from "@/components/NavBar";
 import HeroSection from "@/components/HeroSection";
-
+import SkillsSection from "@/components/SkillsSection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center overflow-hidden mx-auto sm:px-10 px-5 min-h-screen bg-black-100">
+    //<main className="relative overflow-hidden flex justify-center  sm:px-10 px-5 min-h-screen bg-black-100">
+    <main className="flex justify-center bg-black-100">
       < BgVisualEffect /> 
-      <div className="max-w-7xl -mt-14 sm:-mt-10 md:-mt-5 lg:mt-0">
-        {/* <NavBar/> */}
-        
+      <div className="max-w-7xl mt-10 sm:mt-10 md:mt-0 lg:mt-0">
+        {/* <NavBar/> */} 
         <HeroSection/>
-
+        <SkillsSection/>
         {/* <Footer/> */}
-        
       </div>
     </main>
   );
